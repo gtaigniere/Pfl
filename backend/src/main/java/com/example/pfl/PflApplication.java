1674b1988f4a56af3c0096bbccc51be247d551ea
gtaigniere <54893913+gtaigniere@users.noreply.github.com>
2024-05-07 22:45:05 +0200

    Premier commit

---
+++ backend/src/main/java/com/example/pfl/PflApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class PflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PflApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/pfl/PflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
